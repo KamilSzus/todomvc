9bb381c805950510e35d69b0d83b9d388a99a906
KamilSzus <kamil-szustak1@wp.pl>
2022-02-23 21:53:24 +0100

    deleting task

--- src/test/java/todomvc/listAction/Form.java
+++ src/test/java/todomvc/listAction/Form.java
@@ -7,4 +7,6 @@ class Form {
     static final String TASKS_LIST = ".todo-list label";
     static final String COMPLETE_CHECKBOX = "//label[.='{0}']/preceding-sibling::input[@type='checkbox']";
     static final String FILTERS_BY = "//*[@class='filters']//a[.='{0}']";
+    static final String LABEL_WITH_TASK = "//label[.='{0}']";
+    static final String DESTROY_BUTTON = ".destroy";
 }
--- src/test/java/todomvc/listAction/ListAction.java
+++ src/test/java/todomvc/listAction/ListAction.java
@@ -2,12 +2,16 @@ package todomvc.listAction;
 
 import net.serenitybdd.core.steps.UIInteractionSteps;
 import net.thucydides.core.annotations.Step;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 import java.util.Arrays;
 import java.util.List;
 
 import static todomvc.listAction.Form.COMPLETE_CHECKBOX;
+import static todomvc.listAction.Form.DESTROY_BUTTON;
 import static todomvc.listAction.Form.FILTERS_BY;
+import static todomvc.listAction.Form.LABEL_WITH_TASK;
 import static todomvc.listAction.Form.NEW_TODO_TASK;
 import static todomvc.listAction.Form.TASKS_LIST;
 
@@ -38,5 +42,17 @@ public class ListAction extends UIInteractionSteps {
     @Step("filter by '{0}' task")
     public void filterBy(String state) {
         $(FILTERS_BY,state).click();
+    }
+
+    @Step("Delete task witch name '{0}'")
+    public void deleteTask(String task) {
+        WebElement element = $(LABEL_WITH_TASK,task);
+        WebElement destroyButton = $(DESTROY_BUTTON);
+
+        withAction().moveToElement(element)
+                .moveToElement(destroyButton)
+                .click()
+                .perform();
+
     }
 }
